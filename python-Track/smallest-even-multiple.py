dc4ef292738b7d1534c45f189a609d9d7df86f6f
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2023-11-29 18:10:37 +0300

    Added solution for Smallest Even Multiple

--- python-Track/smallest-even-multiple.py
+++ python-Track/smallest-even-multiple.py
@@ -1,9 +1,8 @@
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
-        i=n
-        while True:
-            if i%2 == 0 and i%n == 0:
-                return i
-                break
-            i+=n
+        
+        if n%2 == 0:
+            return n
+        return n*2
+            
         
