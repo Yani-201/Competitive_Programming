b476b4c2a7b64f52cf96122e433acf1f244f9414
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2023-12-20 17:53:03 +0300

    Added solution for Transpose Matrix

---
+++ Progress-Sheet/transpose-matrix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        answer=[]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if len(answer)<=j:
+                    answer.append([matrix[i][j]])
+                else:
+                    answer[j].append(matrix[i][j])
+        return answer
+
+
+        
