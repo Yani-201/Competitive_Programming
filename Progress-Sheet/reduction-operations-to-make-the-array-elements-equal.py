d88cb78afd2f8bdc04e9ee22e9e1edc4a85e7f7f
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2023-12-30 21:21:04 +0300

    Added solution for Reduction Operations to Make the Array Elements Equal

---
+++ Progress-Sheet/reduction-operations-to-make-the-array-elements-equal.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+
+        nums.sort(reverse=True)
+        ops=0
+        for i in range(len(nums)-1):
+            if nums[i]>nums[i+1]:
+                ops+= i+1
+
+        return ops
