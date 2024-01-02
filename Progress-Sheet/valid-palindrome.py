26e10ff5f6daa32140d8beb7ec66c0954199fe53
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-01-02 19:00:22 +0300

    Added solution for Valid Palindrome

---
+++ Progress-Sheet/valid-palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sen=[]
+        for i in s:
+            if i.isalnum():
+                sen.append(i.lower())
+        words="".join(sen)
+
+        j=0
+        k=len(words)-1
+        while k>j:
+            if words[k]!=words[j]:
+                return False
+            else:
+                j+=1
+                k-=1
+        return True        
