de88311e1adeac76849beca491e54ae4fd4e5a76
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-02-08 15:07:32 +0300

    Add solution for Subarray Sums Divisible by K

---
+++ Progress-Sheet/leetcode/subarray-sums-divisible-by-k.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        acc=0
+        prefix=[0]*len(nums)
+        ct=0
+        track=defaultdict(int)
+        for i in range(len(nums)):
+            acc+=nums[i]
+            prefix[i]=acc
+            x=acc%k
+            if x == 0:
+                ct+=1
+            if x in track:
+                ct+=track[x]
+            track[x]+=1
+
+        return ct
+
+            
+                
