aee895e8ab0d8daf7a3ea13019a30dec113957de
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-02-10 15:47:04 +0300

    Add solution for Separate Black and White Balls

--- Progress-Sheet/leetcode/separate-black-and-white-balls.py
+++ Progress-Sheet/leetcode/separate-black-and-white-balls.py
@@ -1,18 +1,18 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
         if len(s)==1: return 0
-
-        read, write, steps = 0, 0, 0
+        read, write = 0, 0, 
+        count=0
 
         while read<len(s):
 
             if s[read]!='1':
-                steps+=abs(read-write)
+                count+=abs(read-write)
                 write+=1
 
             read+=1
 
-        return steps
+        return count
 
         
         
