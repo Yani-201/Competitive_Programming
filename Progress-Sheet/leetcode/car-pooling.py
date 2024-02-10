2aeee977ce58ec38534b80744f6c35105f6bacb8
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-02-10 15:51:53 +0300

    Add solution for Car Pooling

--- Progress-Sheet/leetcode/car-pooling.py
+++ Progress-Sheet/leetcode/car-pooling.py
@@ -5,11 +5,10 @@ class Solution:
             seats[trip[1]]+=trip[0]
             if trip[2]<1000:
                 seats[trip[2]]-=trip[0]
-        maxi=seats[0]
+        _maxi=seats[0]
         for i in range(1,len(seats)):
             seats[i]+=seats[i-1]
-            if seats[i]>maxi:
-                maxi=seats[i]
-        return capacity>=maxi
+            _maxi=max(_maxi, seats[i])
+        return capacity>=_maxi
 
        
