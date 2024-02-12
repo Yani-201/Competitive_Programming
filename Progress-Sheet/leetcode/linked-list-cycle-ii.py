d49726c7ed8a4f14d812170628341e4daf3427a9
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-02-12 16:46:04 +0300

    Add solution for Linked List Cycle II

--- Progress-Sheet/leetcode/linked-list-cycle-ii.py
+++ Progress-Sheet/leetcode/linked-list-cycle-ii.py
@@ -6,14 +6,38 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        current=head
-        visited=set()
-        visited.add(current)
 
-        while current:
-            current=current.next
-            if current in visited:
-                return current
-            visited.add(current)
+        # o(1) implementation, Floyd's part1 and 2 together
+
+        fast=head
+        slow=head
+        flag=False
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+            if fast==slow:
+                flag=True
+                break
+
+        if flag:
+            new=head
+            while new!=fast:
+                new=new.next
+                fast=fast.next
+            return new
+
         return None
+        
+        #o(n) space implementation but solves part1 and part2 together
+
+        # current=head
+        # visited=set()
+        # visited.add(current)
+
+        # while current:
+        #     current=current.next
+        #     if current in visited:
+        #         return current
+        #     visited.add(current)
+        # return None
         
