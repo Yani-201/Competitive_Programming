2b75fe0e61e86bfd691ba4baec81c7bd4726645e
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-02-20 23:11:14 +0300

    Add solution for Decode String

---
+++ Progress-Sheet/leetcode/decode-string.py
@@ -0,0 +1,23 @@
+class Solution:
+    def __init__(self):
+        self.i = 0
+
+    def decodeString(self, s: str) -> str:
+        result = ""
+        while self.i < len(s) and s[self.i] != ']':
+            if s[self.i].isdigit():
+                k = ""
+                while s[self.i].isdigit():
+                    k += s[self.i]
+                    self.i +=1
+                k = int(k)
+                self.i += 1
+                r = self.decodeString(s)
+                result += r * k
+                self.i += 1
+            else:
+                result += s[self.i]
+                self.i += 1
+        return result
+
+
