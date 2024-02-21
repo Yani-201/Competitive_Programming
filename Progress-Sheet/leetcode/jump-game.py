b569a3a132b2cf9aff7dc720d8ed06b6c53d4d39
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-02-21 17:35:20 +0300

    Add solution for Jump Game

---
+++ Progress-Sheet/leetcode/jump-game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        good_ptr = len(nums)-1
+
+        for i in range(len(nums)-2, -1, -1):
+            if i+nums[i] >= good_ptr:
+                good_ptr=i
+
+        if good_ptr == 0:
+            return True
+        return False
