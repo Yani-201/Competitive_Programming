099eec876792237575f83828c33274bffc7dd507
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-02-23 16:41:31 +0300

    Add solution for Predict the Winner

---
+++ Progress-Sheet/leetcode/predict-the-winner.py
@@ -0,0 +1,23 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        def checkwin(start, end, turn):
+            if start == end:
+                return nums[start]
+            if start > end:
+                return 0
+
+            if turn:
+                turn = False
+                startScore = checkwin(start+1, end, turn)
+                endScore = checkwin(start, end-1, turn)
+                return max(startScore + nums[start], endScore+nums[end]) 
+    
+            else:
+                turn = True
+                startScore = checkwin(start+1, end, turn)
+                endScore = checkwin(start, end-1, turn)
+                return min(startScore - nums[start], endScore-nums[end])
+
+
+        return checkwin(0, len(nums)-1, True) >= 0
+        
