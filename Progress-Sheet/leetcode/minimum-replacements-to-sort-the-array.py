aba18847e783d8d06b274cde423a7765ea1fe590
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-02-24 14:57:58 +0300

    Add solution for Minimum Replacements to Sort the Array

--- Progress-Sheet/leetcode/minimum-replacements-to-sort-the-array.py
+++ Progress-Sheet/leetcode/minimum-replacements-to-sort-the-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def minimumReplacement(self, nums: List[int]) -> int:
+        
         operations=0
         current_max=nums[len(nums)-1]
         for i in range(len(nums)-2, -1, -1):
