97b8cfe3cf97c2eb5d0161e374c6f76b755fe69b
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-02-24 14:52:34 +0300

    Add solution for Evaluate Reverse Polish Notation

--- Progress-Sheet/leetcode/evaluate-reverse-polish-notation.py
+++ Progress-Sheet/leetcode/evaluate-reverse-polish-notation.py
@@ -6,10 +6,8 @@ class Solution:
             if tokens[i] in ("+", "-", "*", "/"):
                 value = eval(stack[-2] + tokens[i] + stack.pop())
                 stack.pop()
-
                 value = math.floor(value) if abs(value)==value else math.ceil(value)
                 stack.append(str(value))
-
             else:
                 stack.append(tokens[i])
 
