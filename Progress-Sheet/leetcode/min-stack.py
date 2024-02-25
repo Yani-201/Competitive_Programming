e98b1c01b96e959cb398385338749a2423e7a566
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-02-25 23:20:53 +0300

    Add solution for Min Stack

--- Progress-Sheet/leetcode/min-stack.py
+++ Progress-Sheet/leetcode/min-stack.py
@@ -3,22 +3,19 @@ class MinStack:
     def __init__(self):
         self.stack = []
         self.min = []
-        
 
     def push(self, val: int) -> None:
         self.stack.append(val)
         if len(self.min) == 0 or self.min[-1] >= val:
             self.min.append(val)
         
-
     def pop(self) -> None:
         new = self.stack.pop()
         if self.min[-1] == new:
             self.min.pop()        
 
     def top(self) -> int:
-        return self.stack[-1]
-        
+        return self.stack[-1]  
 
     def getMin(self) -> int:
         return self.min[-1]
