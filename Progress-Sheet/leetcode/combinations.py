f79b2b6c8065bf241df626cf9fc9a330e96486e8
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-02-27 09:43:51 +0300

    Add solution for Combinations

--- Progress-Sheet/leetcode/combinations.py
+++ Progress-Sheet/leetcode/combinations.py
@@ -6,10 +6,27 @@ class Solution:
             if len(path) == k:
                 comb.append(path[:])
                 return
-                # add to list
+
+            # check if there are enough numbers left for path length to be k
+            if (k - len(path)) > (n - first_num + 1):
+                return
+
+            # first kind of implementation
             for num in range(first_num, n+1):
                 path.append(num)
                 backtrack(num+1, path)
                 path.pop()
+
+            # alternative part of implementation
+            # if first_num > n:
+            #     return
+            #             # add nums and call backtrack 
+            # path.append(first_num)
+            # backtrack(first_num+1, path)
+            #             # call backtrac without adding num 
+            # path.pop()
+            # backtrack(first_num+1, path)
+
+
         backtrack(1, [])
         return comb
