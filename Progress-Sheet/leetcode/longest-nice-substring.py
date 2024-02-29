0765721d814ff31a5f65d32a7bb7bde9f0213a05
Yani-201 <112109249+Yani-201@users.noreply.github.com>
2024-03-01 02:38:49 +0300

    Add solution for Longest Nice Substring

---
+++ Progress-Sheet/leetcode/longest-nice-substring.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        if len(s) < 2:
+            return ""
+
+        letters = set(s)
+        for idx, cxr in enumerate(s):
+            if cxr.swapcase() not in letters:
+                left = self.longestNiceSubstring(s[:idx])
+                right = self.longestNiceSubstring(s[idx+1:])
+
+                return right if len(right) > len(left) else left
+
+        return s
+        
